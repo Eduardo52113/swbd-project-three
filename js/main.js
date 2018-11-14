189fe7df878b86aff3d14f6c91722cfaac0b0211
Eduardo Perez <eduardo.perez52113@gmail.com>
2018-11-14 11:00:35 -0600

    updated project three

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
+/*The syntax for JS varible*/
+/* variable keyword varible name = varible value*/
+/*The syntax for JS fucntion*/
+/* function keyword function name {
+  whatever code you want inside the fucntion
+}*/
+/*After creating the fucntion, to call it just do
+ fucntion name(any parameters to pass through else leave blank);
+ parameters are seperate by commas (input1, input2,...)
+ return
+*/
+
+
+/*Their are three data types in JS
+numeric data, string data, Boolean data*/
+
+function sayHello(){
+  document.write('Well, Hello there');
+}
+
+sayHello();
+
 var today = new Date();
 var hourNow = today.getHours();
 var greeting;
-
+/**/
 if (hourNow > 18){
   greeting = 'Good Evening!';
 } else if (hourNow > 12) {
@@ -11,6 +33,5 @@ if (hourNow > 18){
 } else {
   greeting = 'Welcome!'
 }
-
 
 document.write('<h3>' + greeting + '</h3>')
